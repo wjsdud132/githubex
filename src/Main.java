16d6a31f45c33c8e3461bed13ead2b43175d4d6c
Jinyoung <wjsdud132@naver.com>
2024-11-20 19:14:32 +0900

    fixture-1 branch 생성.

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,11 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
-        //이부분 병경 한거에요
-        //이거 풀하기위해서 적어보겠습니다.
-        //ㄴㄹㄴㅇㄹ
-        //ㅇㅈㅈㅇㄷㄹ
-        //이부분 풀 안되어있는데요??
-        //fixture-1입니다
+        /*
+        이부분 병경 한거에요
+        이거 풀하기위해서 적어보겠습니다.
+        fixture-1
+        */
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
