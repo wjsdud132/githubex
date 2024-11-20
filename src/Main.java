e1cd2dae230d3ed6974533269482d9899b7b6196
wjsdud132 <162676001+wjsdud132@users.noreply.github.com>
2024-11-20 18:46:16 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
         //이부분 병경 한거에요
         //이거 풀하기위해서 적어보겠습니다.
         //ㄴㄹㄴㅇㄹ
+        //ㅇㅈㅈㅇㄷㄹ
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
